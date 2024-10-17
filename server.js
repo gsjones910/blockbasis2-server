e09e6de1da3421f4319bbf530629f5ad5e574897
gsjones910 <gsjones910@yandex.com>
2024-10-18 01:03:58 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const { BeehiivClient } = require('@beehiiv/sdk');
+const app = express();
+const port = 3000;
+
+app.use(cors({
+    origin: 'https://scan.blockbasis.com'
+    // origin: '*'
+}));
+
+app.use(express.json());
+
+app.post('/api/beehiiv_getByEmail', async (req, res) => {
+    const { token, publicationId, userEmail } = req.body;
+
+    const client = new BeehiivClient({ token: token });
+    const response = await client.subscriptions.getByEmail(publicationId, userEmail);
+
+    const isPremium = response.data.subscriptionPremiumTierNames.includes('Premium');
+
+    res.json({ isPremium: isPremium });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
